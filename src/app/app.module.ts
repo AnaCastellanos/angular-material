6adc2c4412b735f76bc639453ede8539b6549153
Lemus <oscarl.ocho@gmail.com>
2018-03-26 23:23:26 -0600

    TrainingService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,13 @@ import { PastTrainingsComponent } from './training/past-trainings/past-trainings
 import { WelcomeComponent } from './welcome/welcome.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import {StopTrainingComponent} from './training/current-training/stop-training.component';
+import {AuthService} from './auth/auth.service';
+import {AuthGuard} from './auth/auth.guard';
+import {TrainingService} from './training/training.service';
 
 
 @NgModule({
@@ -40,9 +43,14 @@ import {StopTrainingComponent} from './training/current-training/stop-training.c
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    TrainingService
+  ],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
